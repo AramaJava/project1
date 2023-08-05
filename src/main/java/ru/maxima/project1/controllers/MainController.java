86c50d1cce3f3668a44ea32faddb89add7c47629
AramaJava <abr-aram@yandex.ru>
2023-08-05 07:10:22 +0500

    without valdator

--- src/main/java/ru/maxima/project1/controllers/MainController.java
+++ src/main/java/ru/maxima/project1/controllers/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class MainController {
+    //стартовая страница
     @GetMapping()
     public String index() {
         return "main/index";
